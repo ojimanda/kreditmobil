0b91b220c53c34fd2391e5874724ea0b436936c8
yozimanda <yozimanda20@gmail.com>
2023-01-03 15:21:26 +0700

    add register and login to database

--- app/src/main/java/id/kelompok7/kreditmobil/Login.java
+++ app/src/main/java/id/kelompok7/kreditmobil/Login.java
@@ -3,12 +3,28 @@ package id.kelompok7.kreditmobil;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+
+    EditText txEmail, txPassword;
+    Button btLogin;
+    TextView txRegister;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // initialize variable
+
+        txEmail = findViewById(R.id.etEmail);
+        txPassword = findViewById(R.id.etPassword);
+        btLogin = findViewById(R.id.btLogin);
+        txRegister = findViewById(R.id.txRegister);
+
     }
 }
